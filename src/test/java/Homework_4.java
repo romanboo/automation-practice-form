5a8cad3af2edf55b204d65231865bad0231e87e3
romanboo <romanminarov@gmail.com>
2025-02-03 00:09:25 +0400

    added homework_4

---
+++ src/test/java/Homework_4.java
@@ -0,0 +1,48 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Homework_4 {
+
+    @AfterEach
+    public void logout() {
+        Selenide.closeWebDriver();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+    @Test
+    void сheckingPageLoading() {
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        $("[id=hero-section-brand-heading]").shouldHave(text("The AI-powered\n" +
+                "developer platform"));
+    }
+
+    @Test
+    void dragAndDropTest(){
+       open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to("#column-b"));
+         $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
+
